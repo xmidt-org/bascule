1a5621ea8ae513f558b99236b5b54968e69aa5e5
Kristina Spring <kmspring57@gmail.com>
2019-03-12 09:20:52 -0700

    Added bearer token factory

--- bascule/basculehttp/tokenFactory.go
+++ bascule/basculehttp/tokenFactory.go
@@ -8,6 +8,20 @@ import (
 	"net/http"
 
 	"github.com/Comcast/comcast-bascule/bascule"
+	"github.com/Comcast/comcast-bascule/bascule/key"
+	"github.com/SermoDigital/jose/jws"
+	"github.com/SermoDigital/jose/jwt"
+)
+
+const (
+	jwtPrincipalKey = "sub"
+)
+
+var (
+	ErrorNoProtectedHeader   = errors.New("Missing protected header")
+	ErrorNoSigningMethod     = errors.New("Signing method (alg) is missing or unrecognized")
+	ErrorUnexpectedPayload   = errors.New("Payload isn't a map of strings to interfaces")
+	ErrorUnexpectedPrincipal = errors.New("Principal isn't a string")
 )
 
 // TokenFactory is a strategy interface responsible for creating and validating a secure token
@@ -44,4 +58,73 @@ func (btf BasicTokenFactory) ParseAndValidate(ctx context.Context, _ *http.Reque
 
 	// failed authentication
 	return nil, errors.New("TODO: Enrich this error with information")
+}
+
+type BearerTokenFactory struct {
+	DefaultKeyId  string
+	Resolver      key.Resolver
+	Parser        bascule.JWSParser
+	JWTValidators []*jwt.Validator
+}
+
+func (btf BearerTokenFactory) ParseAndValidate(ctx context.Context, request *http.Request, auth bascule.Authorization, value string) (bascule.Token, error) {
+	decoded, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+		return nil, err
+	}
+
+	jwsToken, err := btf.Parser.ParseJWS(decoded)
+	if err != nil {
+		return nil, err
+	}
+
+	protected := jwsToken.Protected()
+	if len(protected) == 0 {
+		return nil, ErrorNoProtectedHeader
+	}
+
+	alg, _ := protected.Get("alg").(string)
+	signingMethod := jws.GetSigningMethod(alg)
+	if signingMethod == nil {
+		return nil, ErrorNoSigningMethod
+	}
+
+	keyID, _ := protected.Get("kid").(string)
+	if len(keyID) == 0 {
+		keyID = btf.DefaultKeyId
+	}
+
+	pair, err := btf.Resolver.ResolveKey(keyID)
+	if err != nil {
+		return nil, err
+	}
+
+	// validate the signature
+	if len(btf.JWTValidators) > 0 {
+		// all JWS implementations also implement jwt.JWT
+		err = jwsToken.(jwt.JWT).Validate(pair.Public(), signingMethod, btf.JWTValidators...)
+	} else {
+		err = jwsToken.Verify(pair.Public(), signingMethod)
+	}
+
+	if err != nil {
+		// todo: add metrics to log the type of verification error
+		return nil, err
+	}
+
+	payload, ok := jwsToken.Payload().(bascule.Attributes)
+	if !ok {
+		return nil, ErrorUnexpectedPayload
+	}
+
+	principal, ok := payload[jwtPrincipalKey].(string)
+	if !ok {
+		return nil, ErrorUnexpectedPrincipal
+	}
+
+	if caps, capOkay := jwsToken.Payload().(jws.Claims).Get("capabilities").([]interface{}); capOkay && len(caps) > 0 {
+		return bascule.NewToken("sat", principal, jwsToken.Payload().(bascule.Attributes)), nil
+	}
+
+	return bascule.NewToken("jwt", principal, jwsToken.Payload().(bascule.Attributes)), nil
 }
---
+++ bascule/jws.go
@@ -0,0 +1,24 @@
+package bascule
+
+import (
+	"github.com/SermoDigital/jose/jws"
+)
+
+// JWSParser parses raw Tokens into JWS objects
+type JWSParser interface {
+	ParseJWS([]byte) (jws.JWS, error)
+}
+
+type defaultJWSParser int
+
+func (parser defaultJWSParser) ParseJWS(token []byte) (jws.JWS, error) {
+	if jwtToken, err := jws.ParseJWT(token); err == nil {
+		return jwtToken.(jws.JWS), nil
+	} else {
+		return nil, err
+	}
+}
+
+// DefaultJWSParser is the parser implementation that simply delegates to
+// the SermoDigital library's jws.ParseJWT function.
+var DefaultJWSParser JWSParser = defaultJWSParser(0)
