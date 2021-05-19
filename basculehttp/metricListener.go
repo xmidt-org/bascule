b7dd3731a38e1cd6d10dedc3285486a48ca8aa25
Kristina Pathak <kmspring57@gmail.com>
2021-05-18 20:07:01 -0700

    added documentation for metricListener.go

--- basculehttp/metricListener.go
+++ basculehttp/metricListener.go
@@ -32,7 +32,13 @@ const (
 	defaultServer = "primary"
 )
 
-// MetricListener
+// MetricListener keeps track of request authentication and authorization using
+// metrics.  When a request is successful, histograms are updated to mark the
+// time distance from nbf and exp as well as to mark the success in a counter.
+// Upon failure, the counter is incremented to indicate such failure and the
+// reason why.  MetricListener implements the Listener and has an
+// OnErrorResponse function in order for the metrics to be updated at the
+// correct time.
 type MetricListener struct {
 	server    string
 	expLeeway time.Duration
@@ -40,19 +46,27 @@ type MetricListener struct {
 	measures  *AuthValidationMeasures
 }
 
+// Option is how the MetricListener is be configured.
 type Option func(m *MetricListener)
 
+// MetricListenerOptionsIn is an uber fx wired struct that can be used to build
+// a MetricListener.
 type MetricListenerOptionsIn struct {
 	fx.In
 	Measures AuthValidationMeasures
 	Options  []Option `group:"bascule_metric_listener_options"`
 }
 
+// LeewayIn is an uber fx wired struct that provides a bascule leeway, which can
+// be parsed into an Option.
 type LeewayIn struct {
 	fx.In
 	L bascule.Leeway `name:"jwt_leeway" optional:"true"`
 }
 
+// OnAuthenticated is called after a request passes through the constructor and
+// enforcer successfully.  It updates various metrics related to the accepted
+// request.
 func (m *MetricListener) OnAuthenticated(auth bascule.Authentication) {
 	now := time.Now()
 
@@ -99,6 +113,9 @@ func (m *MetricListener) OnAuthenticated(auth bascule.Authentication) {
 	}
 }
 
+// OnErrorResponse is called if the constructor or enforcer have a problem with
+// authenticating/authorizing the request.  The ErrorResponseReason is used as
+// the outcome label value in a metric.
 func (m *MetricListener) OnErrorResponse(e ErrorResponseReason, _ error) {
 	if m.measures == nil {
 		return
@@ -108,18 +125,24 @@ func (m *MetricListener) OnErrorResponse(e ErrorResponseReason, _ error) {
 		Add(1)
 }
 
+// WithExpLeeway provides the exp leeway to be used when calculating the
+// request's offset from the exp time.
 func WithExpLeeway(e time.Duration) Option {
 	return func(m *MetricListener) {
 		m.expLeeway = e
 	}
 }
 
+// WithNbfLeeway provides the nbf leeway to be used when calculating the
+// request's offset from the nbf time.
 func WithNbfLeeway(n time.Duration) Option {
 	return func(m *MetricListener) {
 		m.nbfLeeway = n
 	}
 }
 
+// WithServer provides the server label value to be used by all MetricListener
+// metrics.
 func WithServer(s string) Option {
 	return func(m *MetricListener) {
 		if s != "" {
@@ -128,6 +151,9 @@ func WithServer(s string) Option {
 	}
 }
 
+// NewMetricListener creates a new MetricListener that uses the measures
+// provided and is configured with the given options. The measures cannot be
+// nil.
 func NewMetricListener(m *AuthValidationMeasures, options ...Option) (*MetricListener, error) {
 	if m == nil {
 		return nil, errors.New("measures cannot be nil")
@@ -144,6 +170,8 @@ func NewMetricListener(m *AuthValidationMeasures, options ...Option) (*MetricLis
 	return &listener, nil
 }
 
+// ProvideMetricListener provides the metric listener as well as the options
+// needed for adding it into various middleware.
 func ProvideMetricListener() fx.Option {
 	return fx.Provide(
 		fx.Annotated{
