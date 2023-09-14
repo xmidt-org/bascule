507deff8a64f7fa552821ed3e84065fc0dd71c5b
johnabass <johnbass1017@yahoo.com>
2023-09-14 11:08:05 -0700

    chore: reduce the API surface area a little

--- redesign/bascule/capabilities.go
+++ redesign/bascule/capabilities.go
@@ -1,19 +1,17 @@
 package bascule
 
-// Capabilities is an optional interface that a Token can implement
-// to supply security capabilities.
-type Capabilities interface {
-	// Capabilities returns the slice of security operations this
-	// Token is allowed to be used with.
-	Capabilities() []string
-}
-
 // GetCapabilities returns the set of security capabilities associated
-// with the given Token.  If the Token implements Capabilities, that
-// interface is consulted.  Otherwise, this function returns an
-// empty slice.
+// with the given Token.
+//
+// If the given Token has a Capabilities method that returns a []string,
+// that method is used to determine the capabilities.  Otherwise, this
+// function returns an empty slice.
 func GetCapabilities(t Token) (caps []string) {
-	if c, ok := t.(Capabilities); ok {
+	type capabilities interface {
+		Capabilities() []string
+	}
+
+	if c, ok := t.(capabilities); ok {
 		caps = c.Capabilities()
 	}
 
