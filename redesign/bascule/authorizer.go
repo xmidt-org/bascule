50c35098695c860a9ad0b8a5f620c2185acb268d
johnabass <johnbass1017@yahoo.com>
2023-11-30 17:00:25 -0500

    pruned redesign in favor of v2

--- redesign/bascule/authorizer.go
+++
@@ -1,8 +0,0 @@
-package bascule
-
-// Authorizer is a strategy for verifying that a given Token has
-// access to resources.
-type Authorizer interface {
-	// Authorize verifies that the given Token can access a resource.
-	Authorize(resource any, t Token) error
-}
