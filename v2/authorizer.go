6e57aac6c3a28c25437096581a8ed0040a9d8899
johnabass <johnbass1017@yahoo.com>
2023-11-30 17:03:28 -0500

    chore: licensing

--- v2/authorizer.go
+++ v2/authorizer.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2020 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
 package bascule
 
 // Authorizer is a strategy for verifying that a given Token has
