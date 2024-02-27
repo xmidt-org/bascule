e12d17d508b221723f1cbb35e7d63aa080f839b0
johnabass <johnbass1017@yahoo.com>
2024-02-26 17:43:45 -0800

    chore: added doc.go

---
+++ basculehttp/doc.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+Package basculehttp provides a token-based security workflow for HTTP handlers
+using bascule.
+*/
+package basculehttp
