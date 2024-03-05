2ab6b16046862c21f99761f36814c628ef4f7315
johnabass <johnbass1017@yahoo.com>
2024-03-05 10:43:07 -0800

    chore: license text

--- basculejwt/doc.go
+++ basculejwt/doc.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2021 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
 /*
 Package basculejwt provides JWT support for the bascule workflow.  The canonical
 parser is implemented in terms of https://pkg.go.dev/github.com/lestrrat-go/jwx/v2/jwt#Parse.
