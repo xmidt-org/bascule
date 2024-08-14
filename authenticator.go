6ab6dbd1e78dc8bb5418d9205e67badafc6f4846
johnabass <johnbass1017@yahoo.com>
2024-08-14 11:06:35 -0700

    generic authenticator workflow type

--- authenticator.go
+++ authenticator.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
 package bascule
 
 import "context"
@@ -15,6 +18,61 @@ type AuthenticateEvent[S any] struct {
 	// Err is the error that resulted from authentication.  This field will be
 	// nil for a successful authentication.
 	Err error
+}
+
+// AuthenticatorOption is a configurable option for an Authenticator.
+type AuthenticatorOption[S any] interface {
+	apply(*Authenticator[S]) error
+}
+
+type authenticatorOptionFunc[S any] func(*Authenticator[S]) error
+
+func (aof authenticatorOptionFunc[S]) apply(a *Authenticator[S]) error { return aof(a) }
+
+// WithAuthenticateListeners adds listeners to the Authenticator being built.
+// Multiple calls for this option are cumulative.
+func WithAuthenticateListeners[S any](more ...Listener[AuthenticateEvent[S]]) AuthenticatorOption[S] {
+	return authenticatorOptionFunc[S](
+		func(a *Authenticator[S]) error {
+			a.listeners = a.listeners.Append(more...)
+			return nil
+		},
+	)
+}
+
+// WithTokenParsers adds token parsers to the Authenticator being built.
+// Multiple calls for this option are cumulative.
+func WithTokenParsers[S any](more ...TokenParser[S]) AuthenticatorOption[S] {
+	return authenticatorOptionFunc[S](
+		func(a *Authenticator[S]) error {
+			a.parsers = a.parsers.Append(more...)
+			return nil
+		},
+	)
+}
+
+// WithValidators adds validators to the Authenticator being built.
+// Multiple calls for this option are cumulative.
+func WithValidators[S any](more ...Validator[S]) AuthenticatorOption[S] {
+	return authenticatorOptionFunc[S](
+		func(a *Authenticator[S]) error {
+			a.validators = a.validators.Append(more...)
+			return nil
+		},
+	)
+}
+
+// NewAuthenticator constructs an Authenticator workflow using the supplied options.
+//
+// An empty set of options results in an Authenticator that returns nil Tokens,
+// but no authentication errors.
+func NewAuthenticator[S any](opts ...AuthenticatorOption[S]) (a *Authenticator[S], err error) {
+	a = new(Authenticator[S])
+	for i := 0; err == nil && i < len(opts); i++ {
+		err = opts[i].apply(a)
+	}
+
+	return
 }
 
 // Authenticator provides bascule's authentication workflow.  This type handles
