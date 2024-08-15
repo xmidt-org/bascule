da69b50b92b37ececfd7b9d63092b8f344740ffe
johnabass <johnbass1017@yahoo.com>
2024-08-15 15:05:19 -0700

    simplified workflow setup for HTTP middleware

---
+++ basculehttp/authenticator.go
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2024 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package basculehttp
+
+import (
+	"net/http"
+
+	"github.com/xmidt-org/bascule/v1"
+)
+
+// NewAuthenticator is a convenient wrapper around bascule.NewAuthenticator.
+// This function eases the syntactical pain of generics when creating Middleware.
+func NewAuthenticator(opts ...bascule.AuthenticatorOption[*http.Request]) (*bascule.Authenticator[*http.Request], error) {
+	return bascule.NewAuthenticator(opts...)
+}
